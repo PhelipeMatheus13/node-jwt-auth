45ec8f8bef6a619a2cebbb88281df5a362932803
Phelipe Matheus Andrade <111017521+PhelipeMatheus13@users.noreply.github.com>
2026-04-06 18:35:08 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+/* imports */
+require("dotenv").config()
+const express = require("express")
+const mongose = require("mongoose")
+const brcypt = require("bcrypt")  
+const jwt = require("jsonwebtoken")
+
