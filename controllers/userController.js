3439e21869dc041a4619fe64e8d7659317562436
Phelipe Matheus Andrade <111017521+PhelipeMatheus13@users.noreply.github.com>
2026-04-15 22:46:38 -0300

    Refactor: updating a controller so that it adapts to changes.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,22 @@
-const User = require("../models/User");
+const userService = require("../services/authService");
+
 
 // Get user by ID, Private route
 exports.getUser = async (req, res) => {
     const id = req.params.id;
 
+    if (!id) {
+        return res.status(400).json({ msg: "User ID is required" });
+    }
+
     // Check if user exists
-    const user = await User.findById(id, "-password"); // Exclude password from the response
+    const user = await userService.findUserById(id);
     if (!user) {
         return res.status(404).json({ msg: "User not found" });
     }
 
-    return res.status(200).json(user);
+    // destructuring (rest)
+    const { password, ...userWithoutPassword } = user;
+
+    return res.status(200).json(userWithoutPassword);
 };
