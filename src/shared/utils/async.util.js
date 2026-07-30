18e199a2d548aec8407bac890f7d9776bbfa908e
Phelipe Matheus Andrade <111017521+PhelipeMatheus13@users.noreply.github.com>
2026-07-30 18:12:23 -0300

    refactor: standardizes 4-space indentation in the files
    - exceptions: package-lock.json and package.json use 2-spaces

--- src/shared/utils/async.util.js
+++ src/shared/utils/async.util.js
@@ -2,7 +2,7 @@
 // making error handling more consistent and reducing code duplication(try-catch blocks)
 // capturing any errors that occur in the asynchronous function and passing them to the next middleware
 const asyncHandler = (fn) => (req, res, next) => {
-  Promise.resolve(fn(req, res, next)).catch(next);
+    Promise.resolve(fn(req, res, next)).catch(next);
 };
 
 module.exports = asyncHandler;
